e662483a12a8c26ba5f9dc4cc6e992739e3a95fb
Aravindhan S <149686832+AravindS-Wick@users.noreply.github.com>
2026-06-03 19:06:59 +0530

    feat(react): add react-sparkline component (#32)

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -270,6 +270,8 @@ export type { UseDisclosureReturn } from './hooks';
 export type { Variant, Size, ColorScheme, Placement, ToastPosition } from './types';
 export { cls } from './types';
 
+export { Sparkline } from './Sparkline';
+export type { SparklineProps } from './Sparkline';
 export { Ripple } from './Ripple';
 export type { RippleProps } from './Ripple';
 export { NeonText } from './NeonText';
