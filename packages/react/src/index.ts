97de2292365eebeb219c60f8ecb4d0d39c845298
Aravindhan S <149686832+AravindS-Wick@users.noreply.github.com>
2026-06-03 19:01:44 +0530

    feat(react): add react-masonry component (#28)

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -270,6 +270,8 @@ export type { UseDisclosureReturn } from './hooks';
 export type { Variant, Size, ColorScheme, Placement, ToastPosition } from './types';
 export { cls } from './types';
 
+export { Masonry, MasonryItem } from './Masonry';
+export type { MasonryProps, MasonryItemProps } from './Masonry';
 export { Marquee } from './Marquee';
 export type { MarqueeProps } from './Marquee';
 export { Confetti } from './Confetti';
