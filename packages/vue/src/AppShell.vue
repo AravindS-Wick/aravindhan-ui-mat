632875dc6333b41012571e6d7168c109c56eb88c
Aravindhan S <aravindhanplacements@gmail.com>
2026-06-03 11:33:05 +0530

    feat(vue): add layout components

---
+++ packages/vue/src/AppShell.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { cls } from './types';
+
+const props = withDefaults(defineProps<{
+  mobileToggled?: boolean;
+}>(), {
+  mobileToggled: false,
+});
+
+const className = computed(() =>
+  cls(
+    'av-app-shell',
+    props.mobileToggled && 'av-app-shell--mobile-toggled'
+  )
+);
+</script>
+
+<template>
+  <div :class="className">
+    <slot />
+  </div>
+</template>
