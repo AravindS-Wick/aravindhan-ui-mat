ad300920cd6bf8552ea50343c6f89e63dec9c183
Aravindhan S <149686832+AravindS-Wick@users.noreply.github.com>
2026-06-07 17:48:21 +0530

    feat(vue): add vue-rating component (#46)

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -61,6 +61,7 @@ export { default as ThemeToggle } from './ThemeToggle.vue';
 export type { Variant, Size, ColorScheme, Placement, ToastPosition } from './types';
 export { cls } from './types';
 
+export { default as Rating } from './Rating.vue';
 export { default as Radio } from './Radio.vue';
 export { default as RadioGroup } from './RadioGroup.vue';
 export { default as PasswordInput } from './PasswordInput.vue';
