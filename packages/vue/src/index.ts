c93525d58b47baa93ce21ee3e31e09561eeab9f9
Aravindhan S <149686832+AravindS-Wick@users.noreply.github.com>
2026-06-07 17:42:20 +0530

    feat(vue): add vue-otp-input component (#43)

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -61,6 +61,7 @@ export { default as ThemeToggle } from './ThemeToggle.vue';
 export type { Variant, Size, ColorScheme, Placement, ToastPosition } from './types';
 export { cls } from './types';
 
+export { default as OTPInput } from './OTPInput.vue';
 export { default as NumberInput } from './NumberInput.vue';
 export { default as InputGroup } from './InputGroup.vue';
 export { default as InputGroupText } from './InputGroupText.vue';
